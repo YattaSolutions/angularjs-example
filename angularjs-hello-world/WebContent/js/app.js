5246e8bdcdce443291f3a814a8540ff438b7d0c6
Andreas Scharf <scharf@yatta.de>
2016-01-14 11:06:21 +0100

    Initial commit of AngularJS "Hello World"

---
+++ angularjs-hello-world/WebContent/js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* App Module */
+
+var helloWorldApp = angular.module('helloWorldApp', [
+    'ngRoute',     
+]);
+
+
+//helloWorldApp.config(['$routeProvider', '$locationProvider',
+//    function($routeProvider, $locationProvider) {
+//        $routeProvider.
+//                when('/', {
+//                    templateUrl: 'partials/main.html',
+//                    controller: 'MainCtrl'
+//                }).when('/show', {
+//                    templateUrl: 'partials/show.html',
+//                    controller: 'ShowCtrl'
+//                });
+//
+//        $locationProvider.html5Mode(false).hashPrefix('!');
+//    }]);
+
+
+
